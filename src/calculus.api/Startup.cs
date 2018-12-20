35e1ca2e6a1164fc6c0d1f859239800381098033
Chris Ormerod <chris@chrisormerod.com>
2018-12-20 11:35:31 +1100

    Added Swagger

--- src/calculus.api/Startup.cs
+++ src/calculus.api/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace calculus.api
 {
@@ -26,6 +29,15 @@ namespace calculus.api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+              c.SwaggerDoc("v1", new Info { Title = "Calculus API", Version = "v1" });
+              // Set the comments path for the Swagger JSON and UI.
+              var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+              var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+              c.IncludeXmlComments(xmlPath);                
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,9 +49,17 @@ namespace calculus.api
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+              // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+              app.UseHsts();
             }
+
+
+            // Enable Swagger UI
+            app.UseSwagger();
+            app.UseSwaggerUI(config => {
+              config.SwaggerEndpoint("/swagger/v1/swagger.json", "Calculus API V1");
+              config.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
